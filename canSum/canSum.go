433974bb871221e4c5048e2acf5369d57118e0a2
Matthew Chapman <mttchpmn07@gmail.com>
2021-02-19 16:52:27 -0600

    Add some more test cases for canSum. All passed, call this a success for canSum.

--- canSum/canSum.go
+++ canSum/canSum.go
@@ -56,6 +56,9 @@ func canSumMem(targetSum int, numbers []int, memo map[int]bool) bool {
 }
 
 func main() {
-	fmt.Println(canSumMem(7, []int{5, 3, 4, 7}, make(map[int]bool)))
-	fmt.Println(canSumMem(351, []int{2, 4, 6}, make(map[int]bool)))
+	fmt.Println(canSumMem(7, []int{2, 3}, make(map[int]bool)))       // true
+	fmt.Println(canSumMem(7, []int{5, 3, 4, 7}, make(map[int]bool))) // true
+	fmt.Println(canSumMem(7, []int{2, 4}, make(map[int]bool)))       // false
+	fmt.Println(canSumMem(8, []int{2, 3, 5}, make(map[int]bool)))    // true
+	fmt.Println(canSumMem(300, []int{7, 14}, make(map[int]bool)))    // false
 }
