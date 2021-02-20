2fe7675947b4359fccabbba09429eeb800f16f41
Matthew Chapman <mttchpmn07@gmail.com>
2021-02-20 11:14:46 -0600

    Add bestSum problem definition.

--- bestSum/bestSum.go
+++ bestSum/bestSum.go
@@ -1,5 +1,17 @@
 package main
 
+/*
+				bestSum
+
+	>	Write a function `bestSum(targetSum, numbers)` that takes in a targetSum and an array of number as arguments.
+	>	The function should return an array containing the shortest combination of numbers that add up to exactly the targetSum.
+	>	If there is no combination that adds up to the targetSum, then return nil.
+	>	If there is a tie for the shortest combination, you may return any one of the shortest.
+	>	You may use an element of the array as many times as needed.
+	>	You may assume that all input numbers are nonnegative.
+
+*/
+
 func main() {
 
 }
