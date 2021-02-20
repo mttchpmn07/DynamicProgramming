4eaae89d8ccc15fabb8c12fe837b987285816c66
Matthew Chapman <mttchpmn07@gmail.com>
2021-02-20 16:33:54 -0600

    Add folder for canContruct problem.

---
+++ canConstruct/canContruct.go
@@ -0,0 +1,13 @@
+package main
+
+/*
+				canConstruct
+
+	>	Write a function `canConstruct(target, wordBank)` that accepts a target string and an array of strings.
+	>	The function should return a boolean indicating whether or not the `target` can be constructed by concatenating elements of the `wordBank` array.
+
+*/
+
+func main() {
+
+}
---
+++ canConstruct/go.mod
@@ -0,0 +1,3 @@
+module github.com/mttchpmn07/DynamicProgramming/canConstruct
+
+go 1.15
