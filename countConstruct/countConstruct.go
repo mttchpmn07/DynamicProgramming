2a1de823021aa6be5336e36ff36c2329df1f3108
Matthew Chapman <mttchpmn07@gmail.com>
2021-02-27 10:26:34 -0600

    Working countContruct with memo.

--- countConstruct/countConstruct.go
+++ countConstruct/countConstruct.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 /*
 				countConstruct
 
@@ -9,6 +14,49 @@ package main
 
 */
 
-func main() {
+func countConstructMemo(target string, wordBank []string, memo map[string]int) int {
+	if i, ok := memo[target]; ok {
+		return i
+	}
+	if len(target) == 0 {
+		return 1
+	}
+	sum := int(0)
+	for _, word := range wordBank {
+		if strings.HasPrefix(target, word) {
+			memo[target] = countConstructMemo(strings.TrimPrefix(target, word), wordBank, memo)
+			sum += memo[target]
+		}
+	}
+	memo[target] = sum
+	return sum
+}
 
+func countConstruct(target string, wordBank []string) int {
+	if len(target) == 0 {
+		return 1
+	}
+	sum := int(0)
+	for _, word := range wordBank {
+		if strings.HasPrefix(target, word) {
+			sum += countConstruct(strings.TrimPrefix(target, word), wordBank)
+		}
+	}
+	return sum
+}
+
+func main() {
+	fmt.Println(countConstruct("abcdef", []string{"ab", "abc", "cd", "def", "abcd", "ef"}))
+	fmt.Println(countConstruct("", []string{"ab", "abc", "cd", "def", "abcd"}))
+	fmt.Println(countConstruct("abcdef", []string{}))
+	fmt.Println(countConstruct("enterapotentpot", []string{"a", "p", "ent", "enter", "ot", "o", "t"}))
+	fmt.Println(countConstructMemo("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", []string{
+		"e",
+		"ee",
+		"eee",
+		"eeeee",
+		"eeeeee",
+		"eeeeeee",
+		"eeeeeeee",
+	}, make(map[string]int)))
 }
